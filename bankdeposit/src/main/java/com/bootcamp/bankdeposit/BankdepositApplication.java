864c4fa79b57e7fc0d63205036cba2846afb3e4f
pwhuanco <pedrohuanco@gmail.com>
2022-03-10 22:23:51 -0500

    primera subida

---
+++ bankdeposit/src/main/java/com/bootcamp/bankdeposit/BankdepositApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankdeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankdepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankdepositApplication.class, args);
+	}
+
+}
---
+++ bankdeposit/src/test/java/com/bootcamp/bankdeposit/BankdepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankdeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
