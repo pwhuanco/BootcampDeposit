17849ae080368ca92ab7dd8733c06ebee68b0eed
pwhuanco <pedrohuanco@gmail.com>
2022-03-29 10:09:27 -0500

    correcion de test

--- bankdeposit/src/test/java/com/bootcamp/bankdeposit/service/DepositServiceImplTest.java
+++ bankdeposit/src/test/java/com/bootcamp/bankdeposit/service/DepositServiceImplTest.java
@@ -29,7 +29,7 @@ class DepositServiceImplTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        //fluxDto = Flux.just(new Deposit("098765432", 13100.00, "USD", "007", "", "876543222", "Pepe", "8765432", ""));
+        fluxDto = Flux.just(new Deposit("098765432", 13100.00, "USD", "007", "", "12345", "9876543", "67890", "", ""));
 
         //depositDtoMono = Mono.just(new DepositDto(13100.00, "USD", "007", "", "876543222", "Pepe", "8765432"));
     }
